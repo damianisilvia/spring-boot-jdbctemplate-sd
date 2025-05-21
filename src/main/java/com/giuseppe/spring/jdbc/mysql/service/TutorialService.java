a08e02dad979a5949657e348cd0516c37c6f2ee2
damianisilvia <damianisilvia1@gmail.com>
2025-05-21 10:32:16 +0200

    Refactored controller: removed @Autowired, added constructor injection, introduced TutorialService

---
+++ src/main/java/com/giuseppe/spring/jdbc/mysql/service/TutorialService.java
@@ -0,0 +1,108 @@
+package com.giuseppe.spring.jdbc.mysql.service;
+
+import com.giuseppe.spring.jdbc.mysql.model.Tutorial;
+import com.giuseppe.spring.jdbc.mysql.repository.TutorialRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TutorialService {
+
+    private final TutorialRepository tutorialRepository;
+
+    public TutorialService(TutorialRepository tutorialRepository) {
+        this.tutorialRepository = tutorialRepository;
+    }
+
+    public ResponseEntity<List<Tutorial>> getAllTutorials(String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<>();
+
+            if (title == null)
+                tutorials.addAll(tutorialRepository.findAll());
+            else
+                tutorials.addAll(tutorialRepository.findByTitleContaining(title));
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Tutorial> getTutorialById(long id) {
+        Tutorial tutorial = tutorialRepository.findById(id);
+
+        if (tutorial != null) {
+            return new ResponseEntity<>(tutorial, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<String> createTutorial(Tutorial tutorial) {
+        try {
+            tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            return new ResponseEntity<>("Tutorial was created successfully.", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<String> updateTutorial(long id, Tutorial tutorial) {
+        Tutorial _tutorial = tutorialRepository.findById(id);
+
+        if (_tutorial != null) {
+            _tutorial.setId(id);
+            _tutorial.setTitle(tutorial.getTitle());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+
+            tutorialRepository.update(_tutorial);
+            return new ResponseEntity<>("Tutorial was updated successfully.", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Cannot find Tutorial with id=" + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<String> deleteTutorial(long id) {
+        try {
+            int result = tutorialRepository.deleteById(id);
+            if (result == 0) {
+                return new ResponseEntity<>("Cannot find Tutorial with id=" + id, HttpStatus.OK);
+            }
+            return new ResponseEntity<>("Tutorial was deleted successfully.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Cannot delete tutorial.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<String> deleteAllTutorials() {
+        try {
+            int numRows = tutorialRepository.deleteAll();
+            return new ResponseEntity<>("Deleted " + numRows + " Tutorial(s) successfully.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Cannot delete tutorials.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<Tutorial>> findByPublished() {
+        try {
+            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
